a422d638393278ed246ea871a697c43379a35cdc
alecan27 <alazar.damena01@gmail.com>
2024-02-23 18:31:31 +0300

    newer

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -3,6 +3,7 @@ import 'swiper/swiper-bundle.css';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; */
 /* import React from 'react';
 import './CircularCarousel.css'; */
+import Palm from '../assets/palm.png'
 import "../assets/color.css"
 function Teams() {
 
@@ -15,17 +16,21 @@ function Teams() {
         <div className="grid md:grid-cols-6 grid-cols-10">
         <div className="carousel w-full md:col-start-2 md:col-span-4 col-start-2 col-span-8">
           <div id="slide1" className="carousel-item relative w-full">
-            <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" alt="img1" className="w-full lg:h-[400px] md:h-[300px] h-[200px]" />
+            {/* <img src={Palm} alt="img1" className="w-full lg:h-[400px] md:h-[300px] h-[200px]" /> */}
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide4" className="btn btn-circle">❮</a>
-              <a href="#slide2" className="btn btn-circle">❯</a>
+              <a href="#slide4" className="btn btn-circle text-white">❮</a>
+              <a href="#slide2" className="btn btn-circle text-white">❯</a>
             </div>
-            <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 top-3/4">
-              <div className="grid grid-cols-1">
-                <h2 className="text-black text-3xl">new text</h2>
-                <p className="text-black text-1xl">discription text here....</p>
-              </div>
-            </div>
+            <div className="card card-side bg-base-100 shadow-xl w-full absolute">
+  <figure><img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie"/></figure>
+  <div className="card-body">
+    <h2 className="card-title">New movie is released!</h2>
+    <p>Click the button to watch on Jetflix app.</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Watch</button>
+    </div>
+  </div>
+</div>
           </div>
           <div id="slide2" className="carousel-item relative w-full">
             <img src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" alt="img2" className="w-full" />
