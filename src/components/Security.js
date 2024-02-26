4e970b690eb4542ebe95a524edc7c9f07eea6215
emran <imranhayredin89@gmail.com>
2024-02-26 19:41:55 +0300

    changes

--- src/components/Security.js
+++ src/components/Security.js
@@ -12,7 +12,7 @@ function Security() {
        <section className="bg-gray-100 py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center">
-          <h3 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
+          <h3 className="text-3xl font-extrabold text-gray-900 sm:text-4xl unreal">
             Your Data is Safe with Us
           </h3>
           <p className="mt-4 text-lg text-gray-600">
